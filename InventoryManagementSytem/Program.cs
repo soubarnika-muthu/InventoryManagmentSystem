894f41d8886403292f8826f12f0322fcc9d3aab3
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-07 18:04:08 +0530

    [soubarnika]Creating Json File

--- InventoryManagementSytem/Program.cs
+++ InventoryManagementSytem/Program.cs
@@ -6,7 +6,8 @@ namespace InventoryManagementSytem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Inventory Management System!");
+            //Inventory.Inventory.JsonDeserilaize();
         }
     }
 }
